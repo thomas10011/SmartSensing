a369601478c11cfc7addd5d219522db2d02a5cf0
thomas <tanhuiri@126.com>
2021-03-08 16:00:53 +0800

    优化代码结构

--- app/src/main/java/cn/edu/whu/smartsensing/service/FileListCallbackService.java
+++ app/src/main/java/cn/edu/whu/smartsensing/service/FileListCallbackService.java
@@ -1,5 +1,7 @@
 package cn.edu.whu.smartsensing.service;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,6 +13,7 @@ import okhttp3.Response;
 
 public class FileListCallbackService implements Callback {
 
+    private static final String tag = "FileListCallbackService";
     private String type;
 
     public FileListCallbackService(String type) {
@@ -20,28 +23,23 @@ public class FileListCallbackService implements Callback {
 
     @Override
     public void onFailure(Call call, IOException e) {
-
+        Log.i(tag, "onFailure: 向服务器查询文件列表失败");
     }
 
     @Override
-    public void onResponse(Call call, Response response) throws IOException {
+    public void onResponse(Call call, Response response) {
         Map<String, String> res;
         try {
             res = com.alibaba.fastjson.JSON.parseArray(
-                    response.body().string()).stream().collect(Collectors.toMap(
+                response.body().string()).stream().collect(Collectors.toMap(
                     o -> o.toString().split(",")[0],
                     o -> o.toString().split(",")[1]
-                    )
+                )
             );
         }
         catch (Exception e) {
             res = null;
         }
-        if (type.equals("acceleration")) {
-            UploadUtil.uploadSensorData(res);
-        }
-        else if (type.equals("audio")) {
-            UploadUtil.uploadAudioData(res);
-        }
+        UploadUtil.uploadSensorData(res, type);
     }
 }
--- app/src/main/java/cn/edu/whu/smartsensing/util/UploadUtil.java
+++ app/src/main/java/cn/edu/whu/smartsensing/util/UploadUtil.java
@@ -39,6 +39,10 @@ import okhttp3.Response;
 public class UploadUtil {
 
     private static final SendFiletoServer sendFiletoServer = SendFiletoServer.getInstance();
+    private static final int TIME_OUT = 60*1000;                          //超时时间为60s*1000 = 1000min
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final OkHttpClient client = new OkHttpClient.Builder().connectTimeout(TIME_OUT, TimeUnit.SECONDS).readTimeout(TIME_OUT,TimeUnit.SECONDS).build();;
+
 
     // upload data
     private static final String filepath = "/storage/emulated/0/Android/data" +
@@ -51,6 +55,8 @@ public class UploadUtil {
             "/cn.edu.whu.smartsensing/files/mc/";
     private static final String TAG = "UploadFile";
     private static final String TAG_sensor = "UploadFile_sensor";
+
+
 
     private static String server = "http://124.156.134.117/";
     // private static String server = "http://host.tanhuiri.cn:19526/";
@@ -67,7 +73,6 @@ public class UploadUtil {
         Log.d("search","getFilesAllName ok in " + path);
         return s;
     }
-
 
     public static void execUploadData(String type) {
         // 要首先查询文件列表
@@ -87,135 +92,105 @@ public class UploadUtil {
         ).start();
     }
 
-    public static void uploadSensorData(@Nullable Map<String, String> fileList) {
+    public static void uploadSensorData(@Nullable Map<String, String> fileList, String type) {
         // 先查询服务器上面的文件列表
         getFilesAllName(filepath_sensor).forEach(
-                file -> {
-                    String fileName = file.replaceAll(filepath_sensor,"");
-                    Log.i("Upload Util", "准备开始上传加速度数据：" + fileName);
-                    String fileLen = String.valueOf(new File(file).length());
-                    // 查询不到文件列表 或者文件列表中不包含该文件 或者包含该文件但大小不一致 则上传该文件
-                    if (fileList == null || !fileList.containsKey(fileName) || !fileList.get(fileName).equals(fileLen) ) {
-                        /*----Add SendFile Code----*/
-                        sendFiletoServer.setTimeOut(350);  //60 means 60 seconds, 120 means 2 minutes
-                        sendFiletoServer.upload(
-                                file, fileName, MediaType.parse("text/csv"),
-                                server + "api/file",
-                                new HashMap<String, String>(){
-                                    {
-                                        put("uid", FileUtil.readFile(filepath, "info"));
-                                        put("type", "acceleration");
-                                    }
-                                }
-                        );
-                    }
-                    else {
-                        Log.i(TAG, "uploadSensorData: " + "文件" + fileName + "在服务器上已存在，略过");
-                    }
-                }
-        );
-    }
-
-
-    public static void uploadAudioData(@Nullable Map<String, String> fileList) {
-        // 先查询服务器上面的文件列表
-
-        getFilesAllName(filepath_audio).forEach(
-                file -> {
-                    String fileName = file.replaceAll(filepath_audio,"");
-                    Log.i("Upload Util", "准备开始上传音频数据：" + fileName);
+            file -> {
+                String fileName = file.replaceAll(type.equals("acceleration") ? filepath_sensor : filepath_audio,"");
+                Log.i("Upload Util", "准备开始上传" + type + "数据：" + fileName);
+                String fileLen = String.valueOf(new File(file).length());
+                // 查询不到文件列表 或者文件列表中不包含该文件 或者包含该文件但大小不一致 则上传该文件
+                if (fileList == null || !fileList.containsKey(fileName) || !fileList.get(fileName).equals(fileLen) ) {
                     /*----Add SendFile Code----*/
                     sendFiletoServer.setTimeOut(350);  //60 means 60 seconds, 120 means 2 minutes
                     sendFiletoServer.upload(
-                            file, fileName, MediaType.parse("text/csv"),
-                            server + "api/file",
-                            new HashMap<String, String>(){
-                                {
-                                    put("uid", FileUtil.readFile(filepath, "info"));
-                                    put("type", "audio");
-                                }
+                        file, fileName, MediaType.parse("text/csv"),
+                        server + "api/file",
+                        new HashMap<String, String>(){
+                            {
+                                put("uid", FileUtil.readFile(filepath, "info"));
+                                put("type", type);
                             }
-                    )                    ;
+                        }
+                    );
                 }
+                else {
+                    Log.i(TAG, "uploadSensorData: " + "文件" + fileName + "在服务器上已存在，略过");
+                }
+            }
         );
     }
 
-    private static final int TIME_OUT = 60*1000;                          //超时时间为60s*1000 = 1000min
-    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
-    private static final OkHttpClient client = new OkHttpClient.Builder().connectTimeout(TIME_OUT, TimeUnit.SECONDS).readTimeout(TIME_OUT,TimeUnit.SECONDS).build();;
-
-
     public static void uploadMcData(List<String> date) {
         new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        //准备文件与传入参数
+            new Runnable() {
+                @Override
+                public void run() {
+                    //准备文件与传入参数
 
-                        RequestBody requestBody = RequestBody.create(JSON, com.alibaba.fastjson.JSON.toJSONString(date));
-                        try {
-                            Log.d(TAG+" requestBody", String.valueOf(requestBody.contentLength()));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    RequestBody requestBody = RequestBody.create(JSON, com.alibaba.fastjson.JSON.toJSONString(date));
+                    try {
+                        Log.d(TAG+" requestBody", String.valueOf(requestBody.contentLength()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-                        Request request = new Request.Builder()
-                                .addHeader("Connection","keep-alive")
-                                .url(server + "api/mc-data/" + FileUtil.readFile(filepath, "info"))
-                                .post(requestBody)
-                                .build();
-                        Log.i(TAG+" request", String.valueOf(request.body()));
+                    Request request = new Request.Builder()
+                            .addHeader("Connection","keep-alive")
+                            .url(server + "api/mc-data/" + FileUtil.readFile(filepath, "info"))
+                            .post(requestBody)
+                            .build();
+                    Log.i(TAG+" request", String.valueOf(request.body()));
 
-                        Response response = null;
-                        try {
-                            response = client.newCall(request).execute();
-                            String responseData = response.body().string();
-                            Log.i(TAG, responseData);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                    Response response = null;
+                    try {
+                        response = client.newCall(request).execute();
+                        String responseData = response.body().string();
+                        Log.i(TAG, responseData);
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
+            }
         ).start();
     }
 
     public static void uploadMcFile() {
         // 始终上传更新
         getFilesAllName(filepath_mc).forEach(
-                file -> {
-                    String fileName = file.replaceAll(filepath_mc,"");
-                    Log.i("Upload Util", "准备开始上传mc数据：" + fileName);
-                    /*----Add SendFile Code----*/
-                    sendFiletoServer.setTimeOut(350);  //60 means 60 seconds, 120 means 2 minutes
-                    sendFiletoServer.upload(
-                            file, fileName, MediaType.parse("text/csv"),
-                            server + "api/file",
-                            new HashMap<String, String>(){
-                                {
-                                    put("uid", FileUtil.readFile(filepath, "info"));
-                                    put("type", "mc");
-                                }
-                            }
-                    );
-                }
+            file -> {
+                String fileName = file.replaceAll(filepath_mc,"");
+                Log.i("Upload Util", "准备开始上传mc数据：" + fileName);
+                /*----Add SendFile Code----*/
+                sendFiletoServer.setTimeOut(350);  //60 means 60 seconds, 120 means 2 minutes
+                sendFiletoServer.upload(
+                    file, fileName, MediaType.parse("text/csv"),
+                    server + "api/file",
+                    new HashMap<String, String>(){
+                        {
+                            put("uid", FileUtil.readFile(filepath, "info"));
+                            put("type", "mc");
+                        }
+                    }
+                );
+            }
         );
     }
-
 
     public static void getMcData()  {
 
         new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Request request = new Request.Builder()
-                                .addHeader("Connection","keep-alive")
-                                .url(server + "api/mc-data/" + FileUtil.readFile(filepath, "info"))
-                                .get()
-                                .build();
-                        client.newCall(request).enqueue(new McDataCallbackService());
-                    }
+            new Runnable() {
+                @Override
+                public void run() {
+                    Request request = new Request.Builder()
+                            .addHeader("Connection","keep-alive")
+                            .url(server + "api/mc-data/" + FileUtil.readFile(filepath, "info"))
+                            .get()
+                            .build();
+                    client.newCall(request).enqueue(new McDataCallbackService());
                 }
+            }
         ).start();
     }
 
